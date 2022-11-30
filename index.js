d27f890069d3038b827d732b469ad6d567f86a0d
The Vinh <tvinh321@gmail.com>
2022-11-30 16:17:12 +0700

    update chrome driver to 108

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const {Builder} = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 
-// This Chrome Driver is for Chrome Browser 107 !!!!
+// This Chrome Driver is for Chrome Browser 108 !!!!
 const service = new chrome.ServiceBuilder('./browser-drivers/chromedriver.exe')
 
 const driver = new Builder()
