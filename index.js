aa4f7a91a714331b36bdcb360c44cdf5215e8048
wendy02062001 <wendy.nguyen262001@gmail.com>
2022-12-17 15:43:20 +0700

    feat(Download): add tests

--- index.js
+++ index.js
@@ -13,11 +13,13 @@ const driver = new Builder()
 const loginTest = require("./testcases/TC-LG");
 const logoutTest = require("./testcases/TC-LO");
 const enrollTest = require("./testcases/TC-ER");
+const downloadTest = require("./testcases/TC-DF");
 
 async function main() {
   await loginTest(driver);
   await logoutTest(driver);
   await enrollTest(driver);
+  await downloadTest(driver);
 }
 
 main();
---
+++ testcases/TC-DF.js
@@ -0,0 +1,57 @@
+const fs = require("fs");
+const { By } = require("selenium-webdriver");
+
+module.exports = async function main(driver) {
+  if (!driver) {
+    console.error("TC-LO Error: Driver Not Found");
+    return;
+  }
+
+  // Get Teacher Account Data
+  const teacherAccount = fs.readFileSync("data/account.txt", "utf-8");
+
+  // Split each lines to an array
+  const textData = teacherAccount.split("\r\n");
+
+  let username = "";
+  let password = "";
+
+  textData.forEach((line) => {
+    const accountName = line.split(":")[0];
+    const accountInfo = line.split(":")[1];
+
+    if (accountName === "student") {
+      username = accountInfo.split(";")[0];
+      password = accountInfo.split(";")[1];
+      return;
+    }
+  });
+
+  const account = {
+    username: username,
+    password: password,
+  };
+
+  //First, login
+  await driver.get("https://hihimoodle.gnomio.com/course/view.php?id=2");
+  await driver.findElement(By.id("username")).sendKeys(account.username);
+  await driver.findElement(By.id("password")).sendKeys(account.password);
+  await driver.findElement(By.id("loginbtn")).click();
+
+  //Download file
+  await driver.findElement(By.css("#module-4 .aalink")).click();
+
+  await driver.manage().window().setRect({ width: 500, height: 720 });
+
+  await driver
+    .wait(async () => {
+      const url = await driver.getCurrentUrl();
+      return url.contains(".pdf");
+    }, 5000)
+    .then(() => {
+      console.error(`TC-DF-001 Passed`);
+    })
+    .catch(() => {
+      console.error(`TC-DF-001 Failed`);
+    });
+};
